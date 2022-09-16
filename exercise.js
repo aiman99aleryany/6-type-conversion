86786c36a40b10519cb774477a713923543b5cf6
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-16 03:38:18 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,11 @@
+// # Exercise 6
+
+// * Convert the `hello` variable to a boolean data type.
+// * Convert the `age` variable to a string data type.
+// * Convert the `isGraduated` variable to a numeric data type
+
+let hello = Boolean("Ciao"); // boolean
+let age = String(18); // string
+let isGraduated = Number(false); // number
+
+console.log(hello, age, isGraduated);
